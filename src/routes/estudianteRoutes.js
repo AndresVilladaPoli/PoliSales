f8d1d602cc1604d0dd18f4e406f26fba865417ea
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2024-09-07 18:02:32 -0500

    core: re create project

--- src/routes/estudianteRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const estudianteController = require('../controllers/estudianteController');
-
-router.get('/estudiantes', estudianteController.getEstudiantes);
-
-module.exports = router;
