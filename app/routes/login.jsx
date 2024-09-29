e3ff4830e4999dde212a82ca2c0b78e51f9ff474
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-09-29 18:29:59 -0500

    update Login styles

--- app/routes/login.jsx
+++ app/routes/login.jsx
@@ -21,7 +21,7 @@ export async function loader({ request }) {
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
         Authorization: `Basic ${Buffer.from(
-          `${APP_CLIENT_ID}:${APP_CLIENT_SECRET}`,
+          `${APP_CLIENT_ID}:${APP_CLIENT_SECRET}`
         ).toString("base64")}`,
       },
       body: new URLSearchParams({
@@ -66,7 +66,7 @@ export async function loader({ request }) {
 
 export default function Login() {
   const data = useLoaderData();
-  const [loading, setLoading] = React.useState(false); // estado del spinner
+  const [loading, setLoading] = React.useState(false); // Estado del spinner
   const redirectSearchParams = new URLSearchParams();
   const logoutSearchParams = new URLSearchParams();
 
@@ -96,24 +96,24 @@ export default function Login() {
       <img
         src="../public/img/logo_poli.png"
         alt="Logo"
-        className="absolute top-4 left-4 w-60 h-30"
+        className="absolute top-4 left-4 w-32 sm:w-60 h-auto" //h-auto: para responsive
       />
 
       {/* Logo Polisales */}
       <img
         src="../public/img/logo_polisales_white.png"
         alt="Logo"
-        className="absolute top-4 right-4 w-40 h-40"
+        className="absolute top-4 right-4 w-24 sm:w-40 h-auto"
       />
 
-      <h1 className="text-8xl font-bold text-white mt-18  mb-8">POLIsales</h1>
+      <h1 className="text-4xl sm:text-8xl font-bold text-white mt-18 mb-8">
+        POLIsales
+      </h1>
 
-      <div className="bg-white shadow-lg rounded-lg p-6 md:w-1/3 w-full z-10 text-center">
-          <h2 className="text-3xl font-semibold text-black mb-4">
-                 Inicia Sesión
-          </h2>
-     
-
+      <div className="bg-white shadow-lg rounded-lg p-4 sm:p-6 md:w-1/3 w-full z-10 text-center">
+        <h2 className="text-2xl sm:text-3xl font-semibold text-black mb-4">
+          Inicia Sesión
+        </h2>
 
         {!data.shouldLogout && (
           <a
