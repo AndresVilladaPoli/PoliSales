bab8d92c443a2098ebb3c8d9eddca2e5bccb84b0
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-09-29 19:15:18 -0500

    update nav bar styles
    add an image to the nav

--- app/layouts/Nav.jsx
+++ app/layouts/Nav.jsx
@@ -5,7 +5,7 @@ const CustomNavLink = ({ to, name }) => (
   <NavLink
     to={`/${to}`}
     className={({ isActive, isPending }) =>
-      `p-2.5 rounded-md transition-colors duration-300 ${
+      `p-2 rounded-md transition-colors duration-300 ${
         isActive && !isPending
           ? "bg-[#cedad3] text-[#1c6b44]"
           : "text-white hover:bg-[#cedad3] hover:text-[#1c6b44]"
@@ -19,12 +19,21 @@ const CustomNavLink = ({ to, name }) => (
 const Nav = () => {
   return (
     <div className="w-full">
-      <header className="w-full bg-[#1c6b44] text-white py-4 shadow-md">
-        <h1 className="text-4xl font-bold text-center">
-          Bienvenido a POLIsales
-        </h1>
+      <header className="relative w-full bg-[#1c6b44] text-white py-4 shadow-md flex justify-center items-center">
+        <div className="w-full sm:w-auto flex justify-center">
+          <h1 className="text-3xl sm:text-4xl font-bold text-center">
+            Bienvenido a POLIsales
+          </h1>
+        </div>
+        <img 
+          src="../public/img/logo_polisales_white.png" 
+          alt="Logo" 
+          className="absolute right-8 top-1/2 transform -translate-y-1/2 h-12 w-12 sm:h-16 sm:w-16 md:h-20 md:w-20" 
+        />
       </header>
-      <nav className="flex justify-around bg-[#1c6b44] py-3">
+
+      {/* add responsive */}
+      <nav className="flex flex-col sm:flex-row justify-around bg-[#1c6b44] py-3 text-sm sm:text-base">
         <CustomNavLink to="" name="Publicaciones" />
         <CustomNavLink to="new" name="Crear publicación" />
         <CustomNavLink to="publications" name="Mis publicaciones" />
@@ -32,7 +41,7 @@ const Nav = () => {
         <CustomNavLink to="me" name="Mi perfil" />
         <Link
           to="/logout"
-          className="text-white bg-[#196844] hover:bg-[#024006] focus:ring-4 focus:ring-[#A3BF3F] font-medium rounded-lg text-sm px-5 py-2.5 transition-colors duration-300"
+          className="text-white bg-[#196844] hover:bg-[#024006] focus:ring-4 focus:ring-[#A3BF3F] font-medium rounded-lg px-4 py-2 mt-2 sm:mt-0 sm:ml-4 transition-colors duration-300"
         >
           Cerrar Sesión
         </Link>
--- app/routes/publications.jsx
+++ app/routes/publications.jsx
@@ -7,10 +7,12 @@ export function loader({ request }) {
 
 export default function Publications() {
   return (
-    <div className="h-screen flex flex-col items-center bg-gray-100">
+    <div className="h-screen flex flex-col items-center bg-[#cedad3]">
       <Nav />
-      <main className="flex-grow mt-2">
-        <h1 className="text-4xl font-bold text-center">Mis publicaciones</h1>
+      <main className="flex-grow mt-6 px-4 sm:px-0 w-full max-w-lg">
+        <h1 className="text-4xl font-bold text-center text-[#1c6b44]">
+          Mis publicaciones
+        </h1>
       </main>
     </div>
   );
