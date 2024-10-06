e20db3803b7f3dcee677372635151841a39b4a5c
Daniel Ramirez Gallo <daniel.ramirez@grupokonecta.com>
2024-10-06 13:18:07 -0500

    refactor: server js

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import { createRequestHandler } from "@remix-run/architect";
 import * as remixServerBuild from "./build/server/index.js";
 
-export const handler = createRequestHandler({
-  build: remixServerBuild,
-  getLoadContext(event) {
-    // use lambda event to generate a context for loaders
-    return {};
-  },
-});
+export const handler = async (event) => {
+  console.log("event", event);
+
+  try {
+    const requestHandler = createRequestHandler({
+      build: remixServerBuild,
+    });
+    return await requestHandler(event);
+  } catch (error) {
+    console.error("error", error);
+    return {
+      statusCode: 500,
+      body: "Internal Server Error",
+    };
+  }
+};
