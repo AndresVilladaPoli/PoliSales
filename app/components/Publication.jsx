027649bc4742e5cac94ecd7ecf455c001533cd51
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-10-21 00:13:17 -0500

    add styles to publication cards

--- app/components/Publication.jsx
+++ app/components/Publication.jsx
@@ -7,31 +7,31 @@ const Publication = ({ publication }) => {
   return (
     <div
       key={publication.id}
-      className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      className="w-full max-w-sm bg-white border border-[#1c6b44] rounded-lg shadow-md"
     >
-      <span className="font-bold text-gray-900 dark:text-white">
+      <span className="block px-4 py-2 text-sm font-bold text-[#1c6b44] bg-[#cedad3] rounded-t-lg">
         {new Date(publication.createdAt * 1000).toLocaleDateString()}
       </span>
       <ImageCarrousel files={publication.images} isUrls={true} />
       <div className="px-5 pb-5">
-        <span className="bg-blue-100 text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+        <span className="bg-[#e1be10] text-[#1c6b44] text-xs font-semibold px-2.5 py-0.5 rounded">
           {publication.category}
         </span>
-        <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+        <h5 className="text-xl font-semibold tracking-tight text-[#1c6b44] mt-2">
           {publication.title}
         </h5>
         <div className="flex items-center mt-2.5 mb-5">
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          <p className="mb-3 font-normal text-gray-700">
             {publication.content}
           </p>
         </div>
-        <div className="flex flex-col">
-          <span className="text-3xl font-bold text-gray-900 dark:text-white">
+        <div className="flex flex-col items-start">
+          <span className="text-3xl font-bold text-[#1c6b44] mb-3">
             ${publication.price}
           </span>
           <a
             href="#"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="text-white bg-[#1c6b44] hover:bg-[#145732] focus:ring-4 focus:outline-none focus:ring-[#1c6b44] font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           >
             Ver detalles
           </a>
@@ -42,3 +42,4 @@ const Publication = ({ publication }) => {
 };
 
 export default memo(Publication);
+
