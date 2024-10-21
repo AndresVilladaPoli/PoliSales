da2918913df0c253a27681f45efd19b16d6f5987
AndresVilladaPoli <andres_villada82202@elpoli.edu.co>
2024-10-21 00:08:46 -0500

    add styles to search bar

--- app/components/SearchInput.jsx
+++ app/components/SearchInput.jsx
@@ -2,14 +2,14 @@ const SearchInput = ({ value, onChange, onSearch }) => (
   <>
     <label
       htmlFor="default-search"
-      className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      className="mb-2 text-sm font-medium text-gray-900 sr-only"
     >
       Buscar
     </label>
-    <div className="relative">
-      <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+    <div className="relative w-full max-w-md mx-auto mb-6">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg
-          className="w-4 h-4 text-gray-500 dark:text-gray-400"
+          className="w-5 h-5 text-[#1c6b44]"
           aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
@@ -27,15 +27,15 @@ const SearchInput = ({ value, onChange, onSearch }) => (
       <input
         type="search"
         id="default-search"
-        className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-[#1c6b44] rounded-lg bg-white focus:ring-[#1c6b44] focus:border-[#1c6b44]"
         placeholder="Ingrese su búsqueda"
         required
         value={value}
         onChange={onChange}
       />
       <button
-        type="submit"
-        className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        type="button"
+        className="text-white absolute right-2.5 bottom-2.5 bg-[#1c6b44] hover:bg-[#145732] focus:ring-4 focus:outline-none focus:ring-[#1c6b44] font-medium rounded-lg text-sm px-4 py-2"
         onClick={onSearch}
       >
         Buscar
@@ -45,3 +45,4 @@ const SearchInput = ({ value, onChange, onSearch }) => (
 );
 
 export default SearchInput;
+
